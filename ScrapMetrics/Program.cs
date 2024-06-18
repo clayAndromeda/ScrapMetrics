b90a9d1f536d01e7d285b59235ec2fd88026861e
clayAndromeda <andromeda4869game@gmail.com>
2024-06-18 09:20:12 +0900

    とりあえずグラフ描画

--- ScrapMetrics/Program.cs
+++ ScrapMetrics/Program.cs
@@ -1,20 +1,43 @@
 ﻿using System.Text.Json;
 using ConsoleAppFramework;
+using ScottPlot;
+using ScottPlot.TickGenerators;
 using ScrapMetrics;
 
 ConsoleApp.Run(args, ReadBackupFile);
 
-static void ReadBackupFile(string path)
+static void ReadBackupFile(string path, string output)
 {
 	// pathのファイルを読み込む
 	var fileText = File.ReadAllText(path);
 	
 	var scrapboxData = JsonSerializer.Deserialize<ScrapboxFormat>(fileText);
 	
-	// デシリアライズされたデータの使用例
-	Console.WriteLine($"Name: {scrapboxData.Name}");
-	Console.WriteLine($"DisplayName: {scrapboxData.DisplayName}");
-	Console.WriteLine($"Exported: {scrapboxData.Exported}");
-	Console.WriteLine($"Number of Users: {scrapboxData.Users.Count}");
-	Console.WriteLine($"Number of Pages: {scrapboxData.Pages.Count}");
+	// scrapboxData.Pages.CreatedをUnix時間からDateTimeに変換して、月ごとに集計する
+	var monthlyPageGroups = scrapboxData.Pages
+		.Select(x => (p: x, DateTimeOffset.FromUnixTimeSeconds(x.Created).DateTime))
+		.GroupBy(x => (x.Item2.Year, x.Item2.Month))
+		.Where(x => x.Key.Year >= 2023)
+		.ToArray();
+	
+	foreach (var monthlyPageGroup in monthlyPageGroups)
+	{
+		Console.WriteLine($"{monthlyPageGroup.Key.Year}-{monthlyPageGroup.Key.Month}: {monthlyPageGroup.Count()} pages");
+	}
+
+	ScottPlot.Plot plot = new();
+	double[] pageCounts = monthlyPageGroups.Select(x => (double)x.Count()).ToArray();
+	var barPlot = plot.Add.Bars(pageCounts);
+	foreach (var bar in barPlot.Bars)
+	{
+		bar.Label = ((int)bar.Value).ToString();
+	}
+
+	Tick[] ticks = monthlyPageGroups
+		.Select(x => $"{x.Key.Year}-{x.Key.Month}")
+		.Select((label, i) => new Tick(i, label))
+		.ToArray();
+	plot.Axes.Bottom.TickGenerator = new NumericManual(ticks);
+	
+	plot.SavePng(output, width: 1400, height: 400);
 }
